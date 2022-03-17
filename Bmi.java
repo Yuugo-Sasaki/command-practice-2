9a61f178f75128207369ae1bbd07bb5ba0e2c6bb
Yuugo Sasaki <yuugo.sasaki.sys21@morijyobi.ac.jp>
2022-03-17 12:05:10 +0900

    Bmi.javaを編集

--- Bmi.java
+++ Bmi.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Bmi {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("身長を入力");
