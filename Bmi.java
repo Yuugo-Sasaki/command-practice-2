1d6803a21d3fae689afb918a5c101662fc5feebe
Yuugo Sasaki <yuugo.sasaki.sys21@morijyobi.ac.jp>
2022-03-17 12:15:06 +0900

    Bmi.javaの修正とREADME.mdの新規作成

--- Bmi.java
+++ Bmi.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Bmi {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("身長を入力");
+        System.out.println("身長を入力してください");
         double sin  = scanner.nextDouble();
-        System.out.println("体重を入力");
+        System.out.println("体重を入力してください");
         double tai = scanner.nextDouble();
 
         double sinm = sin / 100;
@@ -15,8 +15,8 @@ public class Bmi {
 
         double lastresult = ((double)Math.round(result * 10))/10;
         
-        System.out.println(lastresult);
-        System.out.println(lastresult);
+        System.out.println("あなたのBMIは" + lastresult + "です。");
+        System.out.println("あなたのBMIは" + lastresult + "です。");
     }
 }
 //体重÷(身長m^2)
